8f8f4605538bccbf993e91475e029d663f79a734
Muskan Garg <106229155+muskangarg03@users.noreply.github.com>
2022-07-21 13:36:23 +0530

    Pattern- letter L
    This is a pattern printing code in java programming. In this, the pattern of letter L is get printed by using conditional statements, loops and operators.

---
+++ letterL.java
@@ -0,0 +1,20 @@
+
+public class letterL {
+
+	public static void main(String[] args) {
+		int n=7;
+		int i,j;
+		for(i=0;i<n;i++)
+		{
+			for(j=0;j<n;j++)
+			{
+				if(j==0 || i==(n-1))
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
