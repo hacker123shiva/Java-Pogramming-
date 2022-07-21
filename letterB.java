02b0d43c719959debc679f2d800f918208f12fef
Muskan Garg <106229155+muskangarg03@users.noreply.github.com>
2022-07-21 13:12:39 +0530

    Pattern- letter B
    This is a pattern printing code in java programming. In this, the pattern of letter B is get printed by using conditional statements, loops and operators.

---
+++ letterB.java
@@ -0,0 +1,33 @@
+// letter B
+public class letterB {
+
+	public static void main(String[] args) {
+		int n=7;
+		int i,j;
+		for(i=0;i<n;i++)
+		{
+			for(j=0;j<n;j++)
+			{
+				if(i==0 || i==(n-1)/2 ||j==0 || j==(n-1) || i==(n-1))
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		
+		// Upgraded Another font with curves
+		for(i=0;i<n;i++)
+		{
+			for(j=0;j<n;j++)
+			{
+				if(i==0 && j!=(n-1) || i==(n-1)/2 && j!=(n-1) ||j==0 && i!=0 || j==(n-1) && i!=0 && i!=(n-1) && i!=(n-1)/2 || i==(n-1) && j!=(n-1))
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
+}
