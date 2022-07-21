cf29f6f1dc3a264c299c707ff9c98f0b9d1b62fa
Muskan Garg <106229155+muskangarg03@users.noreply.github.com>
2022-07-21 13:19:14 +0530

    Pattern- letter I
    This is a pattern printing code in java programming. In this, the pattern of letter I is get printed by using conditional statements, loops and operators.

---
+++ letterI.java
@@ -0,0 +1,20 @@
+
+public class letterI {
+
+	public static void main(String[] args) {
+		int n=7;
+		int i,j;
+		for(i=0;i<n;i++)
+		{
+			for(j=0;j<n;j++)
+			{
+				if(i==0 || j==(n-1)/2 ||i==(n-1))
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
