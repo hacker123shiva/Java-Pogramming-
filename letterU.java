5c76d2e81d05a4e8d8ced348100af0147bb67991
Muskan Garg <106229155+muskangarg03@users.noreply.github.com>
2022-07-21 13:48:13 +0530

    Pattern- letter U
    This is a pattern printing code in java programming. In this, the pattern of letter U is get printed by using conditional statements, loops and operators.

---
+++ letterU.java
@@ -0,0 +1,35 @@
+// letter U
+public class letterU {
+
+	public static void main(String[] args) {
+		int n=7;
+		int i,j;
+		for(i=0;i<n;i++)
+		{
+			for(j=0;j<n;j++)
+			{
+				if(j==0 || i==(n-1) || j==(n-1))
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		
+		// Upgraded Another font with curves
+		for(i=0;i<n;i++)
+		{
+			for(j=0;j<n;j++)
+			{
+				if(j==0 && i!=(n-1) || i==(n-1) && j!=0 && j!=(n-1) || j==(n-1) && i!=(n-1))
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
